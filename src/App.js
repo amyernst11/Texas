1ab79aa999bd11bdc5621159f9c25b43d66bbc19
Amy Ernst <aje329@nyu.edu>
2019-09-18 12:10:17 +0800

    first update

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
+    <body>
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Deep in the Heart of Texas!</h1>
+      <img width="500" alt="bluebonnets" src="https://www.snopes.com/tachyon/2014/04/bluebonnets..jpg?resize=700,452" ></img>
+      <img className="longhorn" width="500" alt="longhorn" src="https://radioimg.s3.amazonaws.com/kluvfm/styles/delta__775x515/s3/GettyImages-979443546.jpg?ZtwKsaPbmpcb3uI4KDBGdiAvSATw.G7A&itok=_oCBUr6X&c=09ca564f3b6d69d9a39bdea164245d00" ></img>
+      <img className="yellow-rose" width="500" alt="yellow rose" src="https://img.sheetmusic.direct/catalogue/product/smd_h_0000000000724006/large.jpg" ></img>
+   
     </div>
+
+    </body>
   );
 }
 
